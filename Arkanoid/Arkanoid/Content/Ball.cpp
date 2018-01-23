271378f029e6b52019c3ea257614bb0202b94160
DESKTOP-LN20E9F\Riccardo Reffato <riccardo.reffato@gmail.com>
2018-01-23 20:10:18 +0100

    Corretto il rimbalzo della palla

--- Arkanoid/Arkanoid/Content/Ball.cpp
+++ Arkanoid/Arkanoid/Content/Ball.cpp
@@ -55,56 +55,70 @@ void Ball::OnCollision(Physics::PhysicsObject* other, const Physics::ContactPoin
 	DirectX::XMFLOAT3 xAxes(1.0f, 0.0f, 0.0f);
 	DirectX::XMVECTOR xAxesVector = DirectX::XMLoadFloat3(&xAxes);
 
+	DirectX::XMFLOAT3 zAxes(0.0f, 0.0f, 1.0f);
+	DirectX::XMVECTOR zAxesVector = DirectX::XMLoadFloat3(&zAxes);
+
+	DirectX::XMFLOAT3 yAxes(0.0f, 1.0f, 0.0f);
+	DirectX::XMVECTOR yAxesVector = DirectX::XMLoadFloat3(&yAxes);
+
 	DirectX::XMVECTOR xDir = DirectX::XMVector3Dot(v, xAxesVector);
 	DirectX::XMFLOAT3 xDirFloat;
 	DirectX::XMStoreFloat3(
 		&xDirFloat,
 		xDir
 	);
+
+	
 	DirectX::XMFLOAT3 newVel = velocity;
 
 
-	DirectX::XMVECTOR dotProdVector = DirectX::XMVector3Dot(v, n);
-	DirectX::XMFLOAT3 dotProdFloat;
-	DirectX::XMStoreFloat3(
-		&dotProdFloat,
-		dotProdVector
-	);
-
-	DirectX::XMVECTOR proj = DirectX::XMVectorScale(n, 2.0f * dotProdFloat.x);
-
-	DirectX::XMVECTOR res = DirectX::XMVectorSubtract(v, proj);
-
-	DirectX::XMStoreFloat3(
-		&newVel,
-		res
-	);
-
-	/* Bouncing handling */
+	// Bouncing handling 
 	if (other->GetID() == GameData::ID_PLAYER)
 	{
-		if (xDirFloat.x > 0 && GetGlobalPosition().x < other->GetGlobalPosition().x ||
-			xDirFloat.x < 0 && GetGlobalPosition().x > other->GetGlobalPosition().x)
+		float distance = fabs(other->GetGlobalPosition().x - GetGlobalPosition().x);
+		float maxDistance = 2.0f;
+		float maxDegree = 0.1f;
+		float minDegree = 0.8f;
+
+		//(distance, degree) (x, y)
+		float finalDegree = ((distance - maxDistance) / (0 - maxDistance))*maxDegree - ((distance) / (0 - maxDistance))*minDegree;
+
+
+		DirectX::XMVECTOR rotQuat = DirectX::XMQuaternionRotationAxis(zAxesVector, finalDegree);
+
+		if (other->GetGlobalPosition().x < GetGlobalPosition().x)
 		{
-			res = DirectX::XMVectorScale(res, -1.0f);
-			
-			dotProdVector = DirectX::XMVector3Dot(res, n);
-			
-			DirectX::XMStoreFloat3(
-				&dotProdFloat,
-				dotProdVector
-			);
-
-			proj = DirectX::XMVectorScale(n, 2 * dotProdFloat.x);
-
-			res = DirectX::XMVectorSubtract(res, proj);
-			
-			DirectX::XMStoreFloat3(
-				&newVel,
-				res
-			);
-
+			rotQuat = DirectX::XMQuaternionRotationAxis(zAxesVector, -1.0f*finalDegree);
 		}
+
+		DirectX::XMVECTOR resultVector = XMVector3Rotate(n, rotQuat);
+
+		resultVector = DirectX::XMVectorScale(resultVector, 5.0f);
+		
+
+		DirectX::XMStoreFloat3(
+			&newVel,
+			resultVector
+		);
+		
+	}
+	else
+	{
+		DirectX::XMVECTOR dotProdVector = DirectX::XMVector3Dot(v, n);
+		DirectX::XMFLOAT3 dotProdFloat;
+		DirectX::XMStoreFloat3(
+			&dotProdFloat,
+			dotProdVector
+		);
+
+		DirectX::XMVECTOR proj = DirectX::XMVectorScale(n, 2.0f * dotProdFloat.x);
+
+		DirectX::XMVECTOR res = DirectX::XMVectorSubtract(v, proj);
+
+		DirectX::XMStoreFloat3(
+			&newVel,
+			res
+		);
 	}
 	
 
